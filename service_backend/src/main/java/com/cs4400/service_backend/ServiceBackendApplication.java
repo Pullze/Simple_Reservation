cd20e1cd48354d5e013ed47c0e32893786da21b4
Lai <lai@gatech.edu>
2021-12-05 21:42:39 -0500

    init swagger api doc

--- service_backend/src/main/java/com/cs4400/service_backend/ServiceBackendApplication.java
+++ service_backend/src/main/java/com/cs4400/service_backend/ServiceBackendApplication.java
@@ -3,9 +3,10 @@ package com.cs4400.service_backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
-@MapperScan("com.cs4400.service_backend.mapper")
+@EnableOpenApi
 public class ServiceBackendApplication {
 
     public static void main(String[] args) {
--- service_backend/src/main/java/com/cs4400/service_backend/entity/Account.java
+++ service_backend/src/main/java/com/cs4400/service_backend/entity/Account.java
@@ -1,16 +1,21 @@
 package com.cs4400.service_backend.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class Account {
 
+    @ApiModelProperty(value = "Email")
     public String email;
 
+    @ApiModelProperty(value = "First Name")
     public String first_Name;
 
+    @ApiModelProperty(value = "Last Name")
     public String last_Name;
 
+    @ApiModelProperty(value = "Password")
     public String pass;
 
 }
