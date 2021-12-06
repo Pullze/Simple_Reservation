0c835b9809d9538833f98ed3005fa6cd8a2948d4
Eric-Shang <sencourant@outlook.com>
2021-12-06 10:12:49 -0500

    flightmapper interface

--- service_backend/src/main/java/com/cs4400/service_backend/mapper/FlightMapper.java
+++ service_backend/src/main/java/com/cs4400/service_backend/mapper/FlightMapper.java
@@ -1,4 +1,12 @@
 package com.cs4400.service_backend.mapper;
 
+import com.cs4400.service_backend.entity.Flight;
+
+import java.sql.Date;
+import java.sql.Time;
+
 public interface FlightMapper {
+    Flight schedule_flight(String flight_num, String airline_name, String  from_airport , String to_airport,
+                           Time departure_time, Time arrival_time, Date flight_date, int cost, int capacity,
+                           Date current_date);
 }
