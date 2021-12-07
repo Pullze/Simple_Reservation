7a068dee7bbb5f56227f4731d20b2dea66cb3dfb
Eric-Shang <sencourant@outlook.com>
2021-12-07 13:43:02 -0500

    schedule_flight register_customer/owner 4.1

--- service_backend/src/main/java/com/cs4400/service_backend/controller/FlightController.java
+++ service_backend/src/main/java/com/cs4400/service_backend/controller/FlightController.java
@@ -1,29 +1,20 @@
 package com.cs4400.service_backend.controller;
 
-import com.cs4400.service_backend.entity.Flight;
+
 import com.cs4400.service_backend.service.FlightProcess;
 import com.cs4400.service_backend.vo.FlightInfo;
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 @Api (tags = "Flight Controller")
 
