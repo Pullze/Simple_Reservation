711d8ae6806880c85fe87bede5014d105e40513d
Lai <lai@gatech.edu>
2021-12-08 16:35:19 -0500

    add GET all timezones.

--- service_backend/src/main/java/com/cs4400/service_backend/controller/AirportController.java
+++ service_backend/src/main/java/com/cs4400/service_backend/controller/AirportController.java
@@ -1,10 +1,12 @@
 package com.cs4400.service_backend.controller;
 
+import com.cs4400.service_backend.entity.Response;
 import com.cs4400.service_backend.service.AirportProcess;
 import com.cs4400.service_backend.vo.AirportInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +19,25 @@ public class AirportController {
     @Autowired
     private AirportProcess airportProcess;
 
+    /**
+     * Get all airports (Admin)
+     * @return Response containing all airportInfo.
+     */
     @GetMapping(value = "/view_airport")
     @ApiOperation(value = "Get all airports (admin)")
-    public List<AirportInfo> getAirportInfo() {
+    public Response<?> getAirportInfo() {
 
-        return airportProcess.getAirportInfo();
+        List<AirportInfo> result = airportProcess.getAirportInfo();
+        return new Response<>(HttpStatus.OK.value(), "Success", result);
+
+    }
+
+    @GetMapping(value = "/airport/time_zone")
+    @ApiOperation(value = "Get timezome of all Airport")
+    public Response<?> getTimeZones() {
+
+        List<String> result = airportProcess.getTimeZones();
+        return new Response<>(HttpStatus.OK.value(), "Success", result);
 
     }
 
